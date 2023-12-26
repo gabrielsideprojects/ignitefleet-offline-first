bc8bb9c30f4de9880dea5676175293b616210312
Gabriel Menezes <gabrielsideprojects@gmail.com>
2023-12-26 16:05:55 -0300

    Car status component and navigate to departure screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,22 @@
 import { useNavigation } from "@react-navigation/native";
 import { HomeHeader } from "../../components/HomeHeader";
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
+import { CarStatus } from "../../components/CarStatus";
 
 export function Home(){
+    const {navigate} = useNavigation()
+
+    function handleRegisterMovement(){
+        navigate('departure')
+    }
+
+
     return (
     <Container>
         <HomeHeader/>
+        <Content>
+         <CarStatus onPress={handleRegisterMovement}/>
+        </Content>
     </Container>
 
     ) 
